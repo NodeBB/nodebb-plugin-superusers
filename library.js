f68c1f0316f8007ff753f866e6364ace90f82b0c
psychobunny <psycho.bunny@hotmail.com>
2014-09-17 15:20:08 -0400

    list groups in ACP

--- library.js
+++ library.js
@@ -1,6 +1,7 @@
 "use strict";
 
-var plugin = {};
+var plugin = {},
+	groups = module.parent.require('./groups');
 
 plugin.init = function(app, middleware, controllers, callback) {
 	app.get('/admin/group-banning', middleware.admin.buildHeader, renderAdmin);
@@ -21,7 +22,19 @@ plugin.addAdminNavigation = function(header, callback) {
 
 
 function renderAdmin(req, res, next) {
-	res.render('admin/group-banning', {});
+	groups.list({
+		expand: true,
+		showSystemGroups: true,
+		truncateUserList: true
+	}, function(err, groups) {
+		groups = groups.filter(function(group) {
+			return group.name !== 'registered-users' && group.name !== 'guests';
+		});
+		
+		res.render('admin/group-banning', {
+			groups: groups
+		});
+	});
 }
 
 module.exports = plugin;
---
+++ static/templates/admin/group-banning.tpl
@@ -0,0 +1,25 @@
+<h1>Group Banning</h1>
+<hr />
+
+<form>
+	<p>
+		Select a group to gain banning privileges
+	</p><br />
+	<div class="alert alert-info">
+		<p>
+			<select class="form-control" data-field="group-banning">
+				<!-- BEGIN groups -->
+				<option value="{groups.name}">{groups.name}</option>
+				<!-- END groups -->
+			</select>
+		</p>
+	</div>
+</form>
+
+<button class="btn btn-lg btn-primary" id="save">Save</button>
+
+<script>
+	require(['forum/admin/settings'], function(Settings) {
+		Settings.prepare();
+	});
+</script>
